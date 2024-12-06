9c74cf8cf718702c3793299f0a1596306df30709
kcoichy <kcoichy@binghamton.edu>
2024-12-05 21:02:16 -0500

    Create bullet.py

---
+++ template_final_project-master/src/bullet.py
@@ -0,0 +1,16 @@
+# Bullet class for player and enemy bullets
+class Bullet(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.image = pygame.Surface((5, 10))  # Corrected this line
+        self.image.fill(RED)
+        self.rect = self.image.get_rect()
+        self.rect.centerx = x
+        self.rect.top = y
+        self.speed = 5
+
+    def update(self):
+        self.rect.y -= self.speed
+        if self.rect.bottom < 0:
+            self.kill()
+            
